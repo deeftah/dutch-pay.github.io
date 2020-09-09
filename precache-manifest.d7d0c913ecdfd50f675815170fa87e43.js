3665aed9c2d67d00ebd96fb690da71fab3901495
areoh <areoh@amazon.com>
2020-09-09 16:10:36 +0900

    Updates

---
+++ precache-manifest.d7d0c913ecdfd50f675815170fa87e43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd95fd3c7b53d9e84de4b445e84cf00c",
+    "url": "/dutch-pay.github.io/index.html"
+  },
+  {
+    "revision": "5bd98ecc081806029ab5",
+    "url": "/dutch-pay.github.io/static/css/main.f81b6dfd.chunk.css"
+  },
+  {
+    "revision": "d9dfce9bdff92ad14959",
+    "url": "/dutch-pay.github.io/static/js/2.02d9fded.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dutch-pay.github.io/static/js/2.02d9fded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd98ecc081806029ab5",
+    "url": "/dutch-pay.github.io/static/js/main.d1b69172.chunk.js"
+  },
+  {
+    "revision": "9b4582d688374e2b77fa",
+    "url": "/dutch-pay.github.io/static/js/runtime-main.5da66473.js"
+  }
+]);
